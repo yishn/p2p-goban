918d7ba1ab3e623efa9c71baa3d4c3ea5fabb586
Yichuan Shen <shen.yichuan@gmail.com>
2018-11-11 02:33:36 +0100

    Make simple P2P connection

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,63 @@
 import {h, Component} from 'preact'
 import {Goban} from '@sabaki/shudan'
 import ToolBar from './ToolBar.js'
+
+import Peer from 'simple-peer'
 import Board from '../crdt-board.js'
 
 export default class App extends Component {
     constructor(props) {
         super(props)
 
+        this.peer = null
+
         this.state = {
+            error: false,
+            busy: true,
             sign: 1,
             board: new Board()
         }
     }
 
+    componentDidMount() {
+        let initiator = location.hash.length <= 1
+
+        this.peer = new Peer({initiator, trickle: false})
+
+        this.peer.on('signal', signal => {
+            let code = JSON.stringify(signal)
+            prompt('Signal', code)
+        })
+
+        this.peer.on('connect', () => {
+            this.peer.send(JSON.parse(this.state.board.operations))
+            this.setState({busy: false})
+        })
+
+        this.peer.on('data', data => {
+            this.setState(({board}) => {
+                for (let operation of JSON.parse(data)) {
+                    board.pushOperation(operation)
+                }
+
+                return {board}
+            })
+        })
+
+        this.peer.on('error', () => {
+            this.setState({error: true})
+        })
+    }
+
     handleVertexClick(evt, vertex) {
         evt.preventDefault()
 
+        if (this.peer == null) return
+
         this.setState(({board, sign}) => {
-            board.set(vertex, board.get(vertex) !== 0 ? 0 : sign)
+            let operation = board.set(vertex, board.get(vertex) !== 0 ? 0 : sign)
+            this.peer.send(JSON.stringify([operation]))
+
             return {board}
         })
     }
@@ -27,7 +67,7 @@ export default class App extends Component {
     }
 
     render() {
-        let {sign, board} = this.state
+        let {busy, sign, board} = this.state
         let signMap = board.render(19, 19)
         let markerMap = signMap.map(row => row.map(_ => null))
         let currentVertex = board.getCurrentVertex()
@@ -39,6 +79,7 @@ export default class App extends Component {
 
         return h('div', {class: 'main-view'},
             h(Goban, {
+                busy,
                 showCoordinates: true,
                 fuzzyStonePlacement: true,
                 animateStonePlacement: true,
